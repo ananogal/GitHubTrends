306b185163ff9f75d42f8ab92830ab6b3bc28d84
Ana Nogal <ana.nogal@tide.co>
2020-08-11 11:50:20 +0200

    Create Xcode project

---
+++ GitHubTrends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTrends
+//
+//  Created by Ana Nogal on 11/08/2020.
+//  Copyright © 2020 ananogal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
